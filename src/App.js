56b7972fc82724b128b287b8118da902267984a5
ayan <theayan44@gmail.com>
2023-05-23 21:29:23 +0530

    complete options(delivery, dining out, nightlife)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
 import React, { lazy, Suspense } from 'react';
 import LazyLoading from './components/LazyLoading/LazyLoading';
+import Home from './pages/Home/Home';
 const Image = lazy(() => import('./components/Image'));
 function App() {
   return (
     <div className="App">
-      <div className='image-container'>
+      <Home />
+
+
+
+
+
+      {/* <div className='image-container'>
         <Suspense fallback={<LazyLoading />}>
           <Image url={'https://d3dyfaf3iutrxo.cloudfront.net/thumbnail/user/8b600a3b074f4e26af0ac42c99fd257d.jpeg'} />
         </Suspense>
-      </div>
+      </div> */}
 
-      {/* <section>
+      <section>
         <section>
           <section>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem, repellat accusantium sit libero maiores
@@ -526,7 +533,7 @@ function App() {
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem, repellat accusantium sit libero maiores
           accusamus
           dolor beatae, itaque ipsa mollitia eligendi nulla odit ex dolorem voluptate architecto! Dolorem, libero fuga.</p>
-      </section> */}
+      </section>
     </div>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css';
 import delivery from '../../utils/images/delivery.png';
 import deliveryBW from '../../utils/images/deliveryBW.webp';
@@ -9,14 +9,39 @@ import nightlifeBW from '../../utils/images/nightlifeBW.png';
 
 
 function Home() {
+    const [currOption, setCurrOption] = useState("delivery");
+
     return (
         <div className='home-container'>
             <div className='home'>
-                <div className='options'>
-                    <div className='img-div'>
-                        <img src={delivery} alt='Delivery' />
+                <div className='options-container' onClick={() => setCurrOption("delivery")}>
+                    <div className='options'>
+                        <div className='img-div' style={{ backgroundColor: currOption == "delivery" && "rgb(252, 238, 192)" }}>
+                            <img className='option-image' src={currOption == "delivery" ? delivery : deliveryBW} alt='Delivery' />
+                        </div>
+                        <h2 className='option-title' style={{ color: currOption == "delivery" && "rgb(239, 79, 95)" }}>Delivery</h2>
                     </div>
-                    <h2 className=''></h2>
+                    <hr className='option-urderline' style={{ transform: currOption == "delivery" && 'scaleX(1)' }}></hr>
+                </div>
+
+                <div className='options-container' onClick={() => setCurrOption("diningOut")}>
+                    <div className='options'>
+                        <div className='img-div' style={{ backgroundColor: currOption == "diningOut" && "rgb(229, 243, 243)" }}>
+                            <img className='option-image' src={currOption == "diningOut" ? diningOut : diningOutBW} alt='Dining Out' />
+                        </div>
+                        <h2 className='option-title' style={{ color: currOption == "diningOut" && "rgb(239, 79, 95)" }}>Dining Out</h2>
+                    </div>
+                    <hr className='option-urderline' style={{ transform: currOption == "diningOut" && 'scaleX(1)' }}></hr>
+                </div>
+
+                <div className='options-container' onClick={() => setCurrOption("nightlife")}>
+                    <div className='options'>
+                        <div className='img-div' style={{ backgroundColor: currOption == "nightlife" && "rgb(237, 244, 255)" }}>
+                            <img className='option-image' src={currOption == "nightlife" ? nightlife : nightlifeBW} alt='Nightlife' />
+                        </div>
+                        <h2 className='option-title' style={{ color: currOption == "nightlife" && "rgb(239, 79, 95)" }}>Nightlife</h2>
+                    </div>
+                    <hr className='option-urderline' style={{ transform: currOption == "nightlife" && 'scaleX(1)' }}></hr>
                 </div>
             </div>
         </div>
