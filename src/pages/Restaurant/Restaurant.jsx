1f5652c06a25f196aec12b5837698fb63cfb5dfa
ayan <theayan44@gmail.com>
2023-05-30 22:36:31 +0530

    code

--- src/pages/Restaurant/Restaurant.jsx
+++ src/pages/Restaurant/Restaurant.jsx
@@ -1,21 +1,59 @@
 import './Restaurant.css';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import {restaurantDetails} from '../../utils/restaurantDetails/restaurantDetails';
+import { dummyRestaurantsDetails } from '../../utils/restaurantDetails/restaurantDetails';
+import GenerateImage from '../../components/GenerateImage/GenerateImage';
 
 const Restaurant = () => {
     const params = useParams();
+    const [restaurantDetails, setRestaurantDetails] = useState({});
+
     useEffect(() => {
         window.scrollTo(0, 0);
         const restaurantId = params.id;
         const lastDizitOfId = restaurantId.charAt(restaurantId.length - 1);
-        console.log(restaurantDetails[lastDizitOfId]);
+        setTimeout(() => {
+            setRestaurantDetails(dummyRestaurantsDetails[lastDizitOfId]);
+        }, 2000)
     }, [])
 
     return (
         <div className='restaurant-page'>
-            Restaurant name: {params.restaurant}<br />
-            Restaurant ID: {params.id}
+            {
+                Object.keys(restaurantDetails).length > 0 ?
+                    <div className='restaurant-details'>
+                        <div className='restaurant-images'>
+                            <div className='image1'>
+                                <GenerateImage
+                                    url={restaurantDetails.page_data.sections?.SECTION_BASIC_INFO.res_thumb}
+                                    alt={"image1"}
+                                />
+                            </div>
+                            <section className='middle-images'>
+                                <div className='image2'>
+                                    <GenerateImage
+                                        url={restaurantDetails.entities.IMAGES[restaurantDetails.page_data.sections.SECTION_IMAGE_CAROUSEL.entities[0].entity_ids[0]].thumbUrl}
+                                        alt={"image2"}
+                                    />
+                                </div>
+                                <div className='image3'>
+                                    <GenerateImage
+                                        url={restaurantDetails.entities.IMAGES[restaurantDetails.page_data.sections.SECTION_IMAGE_CAROUSEL.entities[0].entity_ids[1]].thumbUrl}
+                                        alt={"image3"}
+                                    />
+                                </div>
+                            </section>
+                            <div className='image4'>
+                                <GenerateImage
+                                    url={restaurantDetails.entities.IMAGES[restaurantDetails.page_data.sections.SECTION_IMAGE_CAROUSEL.entities[0].entity_ids[2]].thumbUrl}
+                                    alt={"image4"}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    :
+                    <div>Loading...</div>
+            }
         </div>
     )
 }
