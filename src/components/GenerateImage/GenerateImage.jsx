2662ad6ba5565089f1bd5394474eb05d979c95be
ayan <theayan44@gmail.com>
2023-06-02 20:55:56 +0530

    finally set the lazy loading on image

--- src/components/GenerateImage/GenerateImage.jsx
+++ src/components/GenerateImage/GenerateImage.jsx
@@ -1,51 +1,28 @@
-import './GenerateImage.css';
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useState } from 'react'
 import defaultImage from '../../utils/images/placeholder.webp';
 import LazyLoading from '../LazyLoading/LazyLoading';
 
 function GenerateImage({ url, alt }) {
-    const [loaded, setLoaded] = useState(false);
-    const imageRef = useRef();
+    const [loading, setLoading] = useState(true);
+
     const replaceImage = (e) => {
-        setLoaded(true);
         e.target.src = defaultImage;
+        setLoading(false);
     }
-    const displayImage = () => {
-        scrollHandler();
+    const displayImage = (e) => {
+        setLoading(false);
     }
-
-    document.addEventListener("scroll", scrollHandler);
-    function scrollHandler() {
-        try {
-            if ((window.pageYOffset + (window.innerHeight - 100) >= imageRef.current.offsetParent.offsetTop)) {
-                setLoaded(true);
-            }
-        }
-        catch (err) {
-            // console.log(err);
-        }
-    }
-
-    useEffect(() => {
-        setTimeout(() => {
-            scrollHandler();
-        }, 1500)
-        return () => {
-            document.removeEventListener("scroll", scrollHandler);
-        }
-    }, [])
 
     return (
-        <div className='lazy-image' ref={imageRef}>
-            {!loaded && <LazyLoading />}
-
-            <img style={{ display: !loaded ? "none" : "block" }}
+        <>
+            {loading && <LazyLoading />}
+            <img style={{ display: loading ? "none" : "block" }}
                 src={url} alt={alt}
-                loading='lazy'
                 onLoad={displayImage}
                 onError={replaceImage}
+            // loading='lazy'
             />
-        </div>
+        </>
     )
 }
 
