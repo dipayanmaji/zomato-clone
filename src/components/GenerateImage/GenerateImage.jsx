109adf395962609ed19f1443aa2ac22e8d828b9c
ayan <theayan44@gmail.com>
2023-06-02 16:09:09 +0530

    lazy image loading

--- src/components/GenerateImage/GenerateImage.jsx
+++ src/components/GenerateImage/GenerateImage.jsx
@@ -1,34 +1,51 @@
-import React, { useEffect, useState } from 'react'
+import './GenerateImage.css';
+import React, { useEffect, useRef, useState } from 'react'
 import defaultImage from '../../utils/images/placeholder.webp';
 import LazyLoading from '../LazyLoading/LazyLoading';
 
 function GenerateImage({ url, alt }) {
     const [loaded, setLoaded] = useState(false);
+    const imageRef = useRef();
     const replaceImage = (e) => {
         setLoaded(true);
         e.target.src = defaultImage;
     }
     const displayImage = () => {
-        setLoaded(true);
+        scrollHandler();
+    }
+
+    document.addEventListener("scroll", scrollHandler);
+    function scrollHandler() {
+        try {
+            if ((window.pageYOffset + (window.innerHeight - 100) >= imageRef.current.offsetParent.offsetTop)) {
+                setLoaded(true);
+            }
+        }
+        catch (err) {
+            // console.log(err);
+        }
     }
 
     useEffect(() => {
         setTimeout(() => {
-            setLoaded(true);
-        }, 3000)
+            scrollHandler();
+        }, 1500)
+        return () => {
+            document.removeEventListener("scroll", scrollHandler);
+        }
     }, [])
 
     return (
-        <>
+        <div className='lazy-image' ref={imageRef}>
             {!loaded && <LazyLoading />}
 
-            <img style={{ display: !loaded ? 'none' : 'block' }}
+            <img style={{ display: !loaded ? "none" : "block" }}
                 src={url} alt={alt}
                 loading='lazy'
                 onLoad={displayImage}
                 onError={replaceImage}
             />
-        </>
+        </div>
     )
 }
 
