aba7c6b5afdd86d4f72bee9eeb7744548ad82208
ayan <theayan44@gmail.com>
2023-07-10 16:11:46 +0530

    write a false get api in App.js for activate server while open the site 1st time

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Restaurant from './pages/Restaurant/Restaurant';
 import NotFound from './pages/NotFound/NotFound';
 import Cart from './pages/Cart/Cart';
+import axios from 'axios';
+import { SERVER_URL } from './utils/config/config';
 
 function App() {
   const [filterType, setFilterType] = useState("");
+
+  //call an api for activate the free backend server
+  useEffect(() => {
+    const activateBackendServer = async () => {
+      await axios.get(SERVER_URL + "/api/auth/users");
+    }
+    activateBackendServer();
+  }, [])
+
   return (
     <div className="App">
       <Routes>
